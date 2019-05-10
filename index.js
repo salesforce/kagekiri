f088d1b40c70bc7533fe72a7c3ae5d4ac41a7c48
Nolan Lawson <nlawson@salesforce.com>
2019-05-10 16:49:58 -0700

    some tests are passing

--- index.js
+++ index.js
@@ -1,12 +1,122 @@
 import postcssSelectorParser from "postcss-selector-parser"
 const parser = postcssSelectorParser()
 
+function addAll(arr1, arr2) {
+  for (let item of arr2) {
+    arr1.push(item)
+  }
+}
+
+function getAllElements(context) {
+  const results = []
+
+  // create the results list in depth-first order
+  for (let element of context.querySelectorAll('*')) {
+    results.push(element)
+    if (element.shadowRoot) {
+      addAll(results, getAllElements(element.shadowRoot))
+    }
+  }
+  return results
+}
+
+// given a list of ast nodes, find the final list and stop when hitting
+// a combinator. for instance "div span > button span strong.foo" should return "strong.foo"
+function getLastNonCombinatorNodes(nodes) {
+  let results = []
+  for (let i = nodes.length - 1; i >= 0; i--) {
+    const node = nodes[i]
+    if (node.type === 'combinator') {
+      break
+    }
+    results.push(node)
+  }
+  return results.reverse()
+}
+
+function getParentIgnoringShadow(element) {
+  if (element.parentElement) {
+    return element.parentElement
+  }
+  const rootNode = element.getRootNode()
+  if (rootNode !== document) {
+    return rootNode.host
+  }
+}
+
+function getFirstMatchingAncestor(element, nodes) {
+  let ancestor = getParentIgnoringShadow(element)
+  while (ancestor) {
+    if (matches(ancestor, { nodes })) {
+      return ancestor
+    }
+
+    ancestor = getParentIgnoringShadow(ancestor)
+  }
+}
+
+function matches(element, ast) {
+  let { nodes } = ast
+  for (let i = nodes.length - 1; i >= 0; i--) {
+    let node = nodes[i]
+    if (node.type === 'class') {
+      if (!element.classList.contains(node.value)) {
+        return false
+      }
+    } else if (node.type === 'tag') {
+      if (element.tagName.toLowerCase() !== node.value.toLowerCase()) {
+        return false
+      }
+    } else if (node.type === 'combinator') {
+      if (node.value === ' ') {
+        // walk all ancestors
+        const ancestorNodes = getLastNonCombinatorNodes(nodes.slice(0, i))
+        const ancestor = getFirstMatchingAncestor(element, ancestorNodes)
+        if (!ancestor) {
+          return false
+        } else {
+          element = ancestor
+          i -= ancestorNodes.length
+        }
+      } else if (node.value === '>') {
+        // walk immediate parent only
+        const ancestorNodes = getLastNonCombinatorNodes(nodes.slice(0, i))
+        const ancestor = getParentIgnoringShadow(element)
+        if (!ancestor || !matches(ancestor, { nodes: ancestorNodes }) ) {
+          return false
+        } else {
+          element = ancestor
+          i -= 1
+        }
+      }
+    }
+  }
+  return true
+}
+
+function getMatchingElements(elements, ast) {
+  const results = []
+  for (let element of elements) {
+    for (let node of ast.nodes) { // multiple nodes here are comma-separated
+      if (matches(element, node)) {
+        results.push(element)
+      }
+    }
+  }
+  return results
+}
+
 function querySelector (selector, context = document) {
   return querySelectorAll(selector, context)[0]
 }
 
 function querySelectorAll(selector, context = document) {
   const ast = parser.astSync(selector)
+  // TODO: optimization: don't get all elements, only the ones that match the last relevant selector,
+  // e.g. for "foo bar baz" just search for "baz"
+  const elements = getAllElements(context)
+
+  return getMatchingElements(elements, ast)
 }
 
 export {
--- test/test.js
+++ test/test.js
@@ -1,28 +1,106 @@
-import { querySelectorAll } from '../index.js'
+import {querySelectorAll, querySelector} from '../index.js'
 import * as assert from 'assert'
 import simpleLight1 from './fixtures/simple1/light.html'
 import simpleShadow1 from './fixtures/simple1/shadow.html'
 
-function withContainer(html, cb) {
-  const container = document.createElement('div')
-
-  container.innerHTML = html
-
-  cb(container)
+function withDom(html, cb) {
+  const iframe = document.createElement('iframe')
+  document.body.appendChild(iframe)
+  const iframeDocument = iframe.contentWindow.document
+  iframeDocument.open('text/html', 'replace')
+  iframeDocument.write(html)
+  iframeDocument.close()
+  try {
+    cb(iframeDocument)
+  } finally {
+    document.body.removeChild(iframe)
+  }
 }
 
-describe('test suite', () => {
-  it('simple1 light', () => {
-    withContainer(simpleLight1, context => {
-      const elements = context.querySelectorAll('.container .button span')
-      assert.deepStrictEqual(elements.length, 1)
+function simplifyElement(element) {
+  return {
+    tagName: element.tagName,
+    classList: [...element.classList]
+  }
+}
+
+function simplifyElements(elements) {
+  return [...elements].map(simplifyElement)
+}
+
+function testSelectors(lightDom, shadowDom, tests) {
+  tests.forEach(({ selector, expected }) => {
+    it('light DOM - qSA', () => {
+      withDom(lightDom, context => {
+        const elements = context.querySelectorAll(selector)
+        assert.deepStrictEqual(simplifyElements(elements), expected)
+      })
     })
+    it('shadow DOM - qSA', () => {
+      withDom(shadowDom, context => {
+        const elements = querySelectorAll(selector, context)
+        assert.deepStrictEqual(simplifyElements(elements), expected)
+      })
+    })
+    it('light DOM - qS', () => {
+      withDom(lightDom, context => {
+        const element = context.querySelector(selector)
+        assert.deepStrictEqual(simplifyElement(element), expected[0])
+      })
+    })
+    it('shadow DOM - qSA', () => {
+      withDom(shadowDom, context => {
+        const element = querySelector(selector, context)
+        assert.deepStrictEqual(simplifyElement(element), expected[0])
+      })
+    })
+  })
+}
+
+describe('basic test suite', function () {
+  this.timeout(120000) // useful for debugging
+
+  describe('simple1', () => {
+    const expected = [
+      {
+        tagName: 'SPAN',
+        classList: ['text']
+      }
+    ]
+    testSelectors(simpleLight1, simpleShadow1, [
+      {
+        selector: '.container .component span',
+        expected
+      },
+      {
+        selector: '.container .component .text',
+        expected
+      },
+      {
+        selector: '.container .component span.text',
+        expected
+      },
+      {
+        selector: '.fake, .container .component span.text',
+        expected
+      },
+      {
+        selector: '.text',
+        expected
+      },
+      {
+        selector: '.component .text',
+        expected
+      },
+      {
+        selector: '.container .text',
+        expected
+      },
+      {
+        selector: '.container span',
+        expected
+      }
+    ])
   })
 
-  it('simple1 shadow', () => {
-    withContainer(simpleLight1, context => {
-      const elements = querySelectorAll('.container .button span', context)
-      assert.deepStrictEqual(elements.length, 1)
-    })
-  })
 })
