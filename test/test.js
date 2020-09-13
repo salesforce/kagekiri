e0d9e66bbfdbb97dcb842c2fe70ff7f05ac82f2d
Ted Conn <tconn@salesforce.com>
2020-09-13 23:25:26 +0200

    test: added more test cases

--- test/test.js
+++ test/test.js
@@ -1100,6 +1100,190 @@ describe('basic test suite', function () {
   })
 
   describe('getElementsByTagNameNS', () => {
-    
+    it('light DOM - returns empty with falsy ns', () => {
+      
+    });
+
+    it('shadow DOM - returns empty with falsy ns', () => {
+      
+    });
+
+    it('light DOM - returns ns matches', () => {
+      const tagName = '*';
+      const namespace = 'http://www.w3.org/2000/svg';
+      const expected = [
+        {
+          tagName: 'svg',
+          classList: []
+        },
+        {
+          tagName: 'defs',
+          classList: []
+        },
+        {
+          tagName: 'linearGradient',
+          classList: []
+        },
+        {
+          tagName: 'stop',
+          classList: []
+        },
+        {
+          tagName: 'stop',
+          classList: []
+        },
+        {
+          tagName: 'circle',
+          classList: []
+        }
+      ]
+      withDom(tagNameLight1, context => {
+        assertResultEqual(tagName, context.getElementsByTagNameNS(namespace, tagName), expected, true)
+      })
+    });
+
+    it('shadow DOM - returns ns matches', () => {
+      const tagName = '*';
+      const namespace = 'http://www.w3.org/2000/svg';
+      const expected = [
+        {
+          tagName: 'svg',
+          classList: []
+        },
+        {
+          tagName: 'defs',
+          classList: []
+        },
+        {
+          tagName: 'linearGradient',
+          classList: []
+        },
+        {
+          tagName: 'stop',
+          classList: []
+        },
+        {
+          tagName: 'stop',
+          classList: []
+        },
+        {
+          tagName: 'circle',
+          classList: []
+        }
+      ]
+      withDom(tagNameShadow1, context => {
+        assertResultEqual(tagName, getElementsByTagNameNS(namespace, tagName, context), expected, true)
+      })
+    });
+
+    it('light DOM - wildcard ns', () => {
+      const tagName = 'span';
+      const namespace = '*';
+      const expected = [
+        {
+          tagName: 'SPAN',
+          classList: []
+        }
+      ]
+      withDom(tagNameLight1, context => {
+        assertResultEqual(tagName, context.getElementsByTagNameNS(namespace, tagName), expected, true)
+      })
+    });
+
+    it('shadow DOM - wildcard ns', () => {
+      const tagName = 'span';
+      const namespace = '*';
+      const expected = [
+        {
+          tagName: 'SPAN',
+          classList: []
+        }
+      ]
+      withDom(tagNameShadow1, context => {
+        assertResultEqual(tagName, getElementsByTagNameNS(namespace, tagName, context), expected, true)
+      })
+    });
+
+    it('light DOM - wildcard tagName', () => {
+      const tagName = "*";
+      const expected = [
+        {
+          tagName: 'HTML',
+          classList: []
+        },
+        {
+          tagName: 'HEAD',
+          classList: []
+        },
+        {
+          tagName: 'BODY',
+          classList: []
+        },
+        {
+          tagName: 'DIV',
+          classList: ['container']
+        },
+        {
+          tagName: 'DIV',
+          classList: ['outer-component']
+        },
+        {
+          tagName: 'DIV',
+          classList: ['wrapper']
+        },
+        {
+          tagName: 'DIV',
+          classList: ['inner-component']
+        },
+        {
+          tagName: 'SPAN',
+          classList: []
+        }
+      ]
+      withDom(tagNameLight1, context => {
+        assertResultEqual(tagName, context.getElementsByTagNameNS('http://www.w3.org/1999/xhtml', tagName), expected, true)
+      })
+    });
+
+    it('shadow DOM - wildcard tagName', () => {
+      const tagName = "*";
+      const expected = [
+        {
+          tagName: 'HEAD',
+          classList: []
+        },
+        {
+          tagName: 'BODY',
+          classList: []
+        },
+        {
+          tagName: 'DIV',
+          classList: ['container']
+        },
+        {
+          tagName: 'OUTER-COMPONENT',
+          classList: []
+        },
+        {
+          tagName: 'DIV',
+          classList: ['wrapper']
+        },
+        {
+          tagName: 'INNER-COMPONENT',
+          classList: []
+        },
+        {
+          tagName: 'SPAN',
+          classList: []
+        },
+        {
+          tagName: 'SCRIPT',
+          classList: []
+        }
+      ]
+      withDom(tagNameShadow1, context => {
+        assertResultEqual(tagName, getElementsByTagNameNS('http://www.w3.org/1999/xhtml', tagName, context), expected, true)
+      })
+    });
   })
 })
