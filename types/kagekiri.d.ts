44b2281f4bcf2aabad49ad371a70bca253e8c930
Ted Conn <tconn@salesforce.com>
2020-09-15 13:38:44 +0200

    feat: adding types

---
+++ types/kagekiri.d.ts
@@ -0,0 +1,3 @@
+// Type definitions for kagekiri
+export function querySelector(selector: string, context?: Node): HTMLElement | null;
+export function querySelectorAll(selector: string, context?: Node): NodeList;
