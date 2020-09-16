3d9501fe17af8c7a8ebb18beea4f9c726f04d49d
Ted Conn <tconn@salesforce.com>
2020-09-16 17:48:00 +0200

    fix: add missing type

--- types/kagekiri.d.ts
+++ types/kagekiri.d.ts
@@ -1,3 +1,4 @@
 // Type definitions for kagekiri
 export function querySelector(selector: string, context?: Node): HTMLElement | null;
 export function querySelectorAll(selector: string, context?: Node): NodeList;
+export function getElementsByClassName(names: string, context?: Node): HTMLCollection;
