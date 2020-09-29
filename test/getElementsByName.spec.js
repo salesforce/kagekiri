c44e13416fb2f2708f8aca616539861dfd9b25ba
Ted Conn <tconn@salesforce.com>
2020-09-29 12:59:27 +0200

    test: adding more test for branch coverage

--- test/getElementsByName.spec.js
+++ test/getElementsByName.spec.js
@@ -8,38 +8,98 @@
 
 import { getElementsByName } from '../src/index.js'
 import { assertResultEqual, withDom } from './utils.js'
+import assert from 'assert'
 
 import nameLight1 from './fixtures/name1/light.html'
 import nameShadow1 from './fixtures/name1/shadow.html'
 
 describe('getElementsByName', () => {
-  const expected = [
-    {
-      tagName: 'INPUT',
-      classList: []
-    }
-  ]
-
-  const name = 'name'
-
   it('light DOM - getElementsByName should return all matching elements', () => {
     withDom(nameLight1, context => {
+      let name = 'contactForm'
+      let expected = [
+        {
+          tagName: 'FORM',
+          classList: []
+        }
+      ]
+      assertResultEqual(name, context.getElementsByName(name), expected, true)
+
+      name = 'firstname'
+      expected = [
+        {
+          tagName: 'INPUT',
+          classList: []
+        }
+      ]
+      assertResultEqual(name, context.getElementsByName(name), expected, true)
+
+      name = 'email'
+      expected = [
+        {
+          tagName: 'INPUT',
+          classList: []
+        }
+      ]
+      assertResultEqual(name, context.getElementsByName(name), expected, true)
+
+      name = 'test'
+      expected = [
+        {
+          tagName: 'BUTTON',
+          classList: []
+        }
+      ]
       assertResultEqual(name, context.getElementsByName(name), expected, true)
     })
   })
 
   it('shadow DOM - getElementsByName should return all matching elements', () => {
     withDom(nameShadow1, context => {
+      let name = 'contactForm'
+      let expected = [
+        {
+          tagName: 'FORM',
+          classList: []
+        }
+      ]
+      assertResultEqual(name, getElementsByName(name, context), expected, true)
+
+      name = 'firstname'
+      expected = [
+        {
+          tagName: 'INPUT',
+          classList: []
+        }
+      ]
+      assertResultEqual(name, getElementsByName(name, context), expected, true)
+
+      name = 'email'
+      expected = [
+        {
+          tagName: 'INPUT',
+          classList: []
+        }
+      ]
+      assertResultEqual(name, getElementsByName(name, context), expected, true)
+
+      name = 'test'
+      expected = [
+        {
+          tagName: 'BUTTON',
+          classList: []
+        }
+      ]
       assertResultEqual(name, getElementsByName(name, context), expected, true)
     })
   })
 })
 
 describe('getElementsByName throws Error', () => {
-  /* const name = 'name'
+  const name = 'name'
 
-  it.x('getElementsByName with a window object', () => {
-    withDom(nameLight1, document => {
+  it('getElementsByName with a window object', () => {
+    withDom(nameShadow1, document => {
       assert.throws(() => {
         getElementsByName(name, document.defaultView)
       },
@@ -48,5 +108,14 @@ describe('getElementsByName throws Error', () => {
         message: 'Provided context must be of type Document or ShadowRoot'
       })
     })
-  }) */
+  })
+})
+
+describe('getElementsByName with a default document context', () => {
+  it('should return no items', () => {
+    withDom(nameShadow1, document => {
+      const name = 'firstname'
+      assertResultEqual(name, getElementsByName(name), [], true)
+    })
+  })
 })
